0f0f435c00cc09c3322a85bf745df340b817003c
Exequiel Wiedermann <EWFWiedermann@gmail.com>
2019-09-16 22:37:47 -0300

    Modificando el yaml y el main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:app_portal/screens/admin.dart';
+import 'package:app_portal/pages/login_page.dart';
 
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Admin(),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        primaryColorBrightness: Brightness.light
+      ),
+      title: 'Corralon Portal SAS',
+      home: LoginPage(),
+    );
+  }
 }
